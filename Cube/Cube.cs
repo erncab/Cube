08955d6451f1d9b37dda75f1ffbd36be0d0bc4b6
erncab@yahoo.com <exitos236942>
2017-03-25 11:39:38 -0400

    Initial commit.

---
+++ Cube/Cube.cs
@@ -0,0 +1,17 @@
+﻿namespace Cube
+{
+    public class Cube
+    {
+        public Cube(int sideLength)
+        {
+            _sideLength = sideLength;
+        }
+
+        public int GetVolume()
+        {
+            return _sideLength * _sideLength * _sideLength;
+        }
+
+        private readonly int _sideLength;
+    }
+}
---
+++ CubeTest/CubeTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeTest
+{
+    [TestClass]
+    public class CubeTest
+    {
+        [TestMethod]
+        public void GetVolume()
+        {
+            // Arrange
+            const int sideLength = 1;
+
+            var cube = new Cube.Cube(sideLength);
+
+            // Act
+            var volume = cube.GetVolume();
+
+            // Assert
+            Assert.AreEqual(volume, 1);
+        }
+    }
+}
